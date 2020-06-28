d12812f8c101ad3affbaed4c87aee6f0fc28a99a
Arham Amouei <arham.amouei@protonmail.com>
2020-06-28 12:52:00 +0430

    update makefile.d

--- src/makefile.d
+++ src/makefile.d
@@ -5,7 +5,7 @@ cspline.o: cspline.c cspline.h config.h
 eam.o: eam.c eam.h config.h forces.h types.h cspline.h base.h potential.h \
  list.h
 forces.o: forces.c forces.h config.h eam.h types.h cspline.h lj.h morse.h \
- base.h potential.h md_ghost.h list.h
+ base.h potential.h md_ghost.h list.h molecule.h
 list.o: list.c list.h config.h
 lj.o: lj.c lj.h config.h potential.h types.h base.h list.h forces.h
 md_ghost.o: md_ghost.c base.h config.h potential.h types.h md_ghost.h
@@ -17,4 +17,4 @@ potential.o: potential.c potential.h config.h types.h base.h array.h \
  list.h eam.h forces.h cspline.h molecule.h
 structure.o: structure.c base.h config.h potential.h types.h molecule.h \
  array.h
-timer.o: timer.c timer.h config.h base.h potential.h types.h
+timer.o: timer.c timer.h config.h types.h base.h potential.h
