5cdeae621bf7e261590bc7c482621ed1c8d6671a
Arham Amouei <arham.amouei@proton.me>
2022-09-17 20:02:33 +0430

    add semi-implicit Euler integrator

--- src/integrators.c
+++ src/integrators.c
@@ -23,6 +23,21 @@
 #include "timer.h"
 #include "general.h"
 
+#define WHAT_IF_THE_PARTICLE_HAS_LEFT_THE_BOX(md, c, i, d, x)                  \
+    if ( ((md)->ns[(d)] == 1) && (((x) < 0.0) || ((x) >= (md)->l[(d)])) )      \
+    {                                                                          \
+        if (!(md)->PBC[(d)])                                                   \
+        {                                                                      \
+            _fmd_cell_remove_atom((md), (c), (i));                             \
+            (md)->SubDomain.NumberOfParticles--;                               \
+            (i)--;                                                             \
+            break;                                                             \
+        }                                                                      \
+        else                                                                   \
+            if ((x) < 0.0) (x) += (md)->l[(d)]; else (x) -= (md)->l[(d)];      \
+    }                                                                          \
+    do {} while (0)
+
 fmd_real_t fmd_dync_getTimeStep(fmd_t *md)
 {
     return md->timestep;
@@ -36,65 +51,59 @@ fmd_real_t fmd_dync_getTime(fmd_t *md)
 static void VelocityVerlet_startStep(fmd_t *md, fmd_bool_t UseThermostat)
 {
     fmd_ituple_t ic;
-    int d;
     fmd_real_t VelocityScale, mass;
     fmd_real_t x;
+    cell_t *c;
+    unsigned i;
 
     if (UseThermostat)
         VelocityScale = sqrt(1 + md->timestep / md->BerendsenThermostatParam *
                         (md->DesiredTemperature / md->GroupTemperature - 1));
 
     LOOP3D(ic, md->SubDomain.ic_start, md->SubDomain.ic_stop)
-    {
-        /* iterate over all particles in cell ic */
-
-        int i=0;
-        cell_t *c = &ARRAY_ELEMENT(md->SubDomain.grid, ic);
-
-        while (i < c->parts_num)
+        for (c = &ARRAY_ELEMENT(md->SubDomain.grid, ic), i=0; i < c->parts_num; i++)
         {
             if (md->ActiveGroup != FMD_GROUP_ALL && c->GroupID[i] != md->ActiveGroup)
-            {
-                i++;
                 continue;
-            }
 
             mass = md->potsys.atomkinds[c->atomkind[i]].mass;
 
-            for (d=0; d<DIM; d++)
+            for (int d=0; d<DIM; d++)
             {
                 if (UseThermostat) VEL(c, i, d) *= VelocityScale;
 
                 VEL(c, i, d) += md->timestep * 0.5 / mass * FRC(c, i, d);
                 x = POS(c, i, d) + md->timestep * VEL(c, i, d);
 
-                if ( (md->ns[d] == 1) && ((x < 0.0) || (x >= md->l[d])) )
-                {
-                    if (!md->PBC[d])
-                    {
-                        _fmd_cell_remove_atom(md, c, i);
-                        md->SubDomain.NumberOfParticles--;
-                        i--;
-                        break;
-                    }
-                    else
-                        if (x < 0.0) x += md->l[d]; else x -= md->l[d];
-                }
+                WHAT_IF_THE_PARTICLE_HAS_LEFT_THE_BOX(md, c, i, d, x);
 
                 POS(c, i, d) = x;
             }
-
-            i++;
         }
-    }
 
+    /* check if the particles must be placed in other cells or subdomains */
     _fmd_refreshGrid(md);
+}
+
+/* updates GroupMomentum & GroupParticlesNum & GroupKineticEnergy & GroupTemperature */
+void updateGroupProperties(fmd_t *md, fmd_rtuple_t MomentumSum, int ParticlesNum, fmd_real_t m_vSqd_Sum)
+{
+    fmd_real_t m_vSqd_SumSum;
+
+    MPI_Reduce(MomentumSum, md->GroupMomentum, DIM, FMD_MPI_REAL, MPI_SUM, RANK0, md->MD_comm);
+
+    MPI_Allreduce(&ParticlesNum, &md->GroupParticlesNum, 1, MPI_INT, MPI_SUM, md->MD_comm);
+
+    MPI_Allreduce(&m_vSqd_Sum, &m_vSqd_SumSum, 1, FMD_MPI_REAL, MPI_SUM, md->MD_comm);
+    md->GroupKineticEnergy = 0.5 * m_vSqd_SumSum;
+
+    md->GroupTemperature = m_vSqd_SumSum / (3.0 * md->GroupParticlesNum * K_BOLTZMANN);
 }
 
 static void VelocityVerlet_finishStep(fmd_t *md)
 {
     fmd_ituple_t ic;
-    fmd_real_t m_vSqd_Sum = 0, m_vSqd_SumSum;
+    fmd_real_t m_vSqd_Sum = 0;
     fmd_real_t mass;
     int ParticlesNum = 0;
     fmd_rtuple_t MomentumSum = {0., 0., 0.};
@@ -122,14 +131,8 @@ static void VelocityVerlet_finishStep(fmd_t *md)
                                    sqrr(VEL(c, i, 2)) );
         }
 
-    MPI_Reduce(MomentumSum, md->GroupMomentum, DIM, FMD_MPI_REAL, MPI_SUM, RANK0, md->MD_comm);
-
-    MPI_Allreduce(&ParticlesNum, &md->GroupParticlesNum, 1, MPI_INT, MPI_SUM, md->MD_comm);
-
-    MPI_Allreduce(&m_vSqd_Sum, &m_vSqd_SumSum, 1, FMD_MPI_REAL, MPI_SUM, md->MD_comm);
-    md->GroupKineticEnergy = 0.5 * m_vSqd_SumSum;
-
-    md->GroupTemperature = m_vSqd_SumSum / (3.0 * md->GroupParticlesNum * K_BOLTZMANN);
+    /* update GroupMomentum & GroupParticlesNum & GroupKineticEnergy & GroupTemperature */
+    updateGroupProperties(md, MomentumSum, ParticlesNum, m_vSqd_Sum);
 }
 
 static void tick(fmd_t *md)
@@ -145,20 +148,81 @@ static void incTime(fmd_t *md)
     tick(md);
 }
 
-void fmd_dync_integrate(fmd_t *md, int GroupID, fmd_real_t duration, fmd_real_t timestep)
+/* implementation of semi-implicit Euler method */
+static void SymplecticEuler_takeOneStep(fmd_t *md)
 {
-    if (!md->ParticlesDistributed) _fmd_matt_distribute(md);
+    fmd_ituple_t ic;
+    int d;
+    fmd_real_t mass, x;
+    cell_t *c;
+    unsigned i;
+    int ParticlesNum = 0;
+    fmd_rtuple_t MomentumSum = {0., 0., 0.};
+    fmd_real_t m_vSqd_Sum = 0;
 
-    if (GroupID != md->ActiveGroup && md->ActiveGroup != FMD_GROUP_ALL)
+    LOOP3D(ic, md->SubDomain.ic_start, md->SubDomain.ic_stop)
+        for (c = &ARRAY_ELEMENT(md->SubDomain.grid, ic), i=0; i < c->parts_num; i++)
+        {
+            if (md->ActiveGroup != FMD_GROUP_ALL && c->GroupID[i] != md->ActiveGroup)
+                continue;
+
+            mass = md->potsys.atomkinds[c->atomkind[i]].mass;
+
+            for (d=0; d<DIM; d++)
+            {
+                VEL(c, i, d) += md->timestep * FRC(c, i, d) / mass;
+                x = POS(c, i, d) + md->timestep * VEL(c, i, d);
+
+                WHAT_IF_THE_PARTICLE_HAS_LEFT_THE_BOX(md, c, i, d, x);
+
+                POS(c, i, d) = x;
+            }
+
+            if (d == DIM)  /* if particle is not removed */
+            {
+                ParticlesNum++;
+
+                fmd_real_t temp = 0.0;
+
+                for (d=0; d<DIM; d++)
+                {
+                    temp += sqrr(VEL(c, i, d));
+                    MomentumSum[d] += mass * VEL(c, i, d);
+                }
+
+                m_vSqd_Sum += mass * temp;
+            }
+        }
+
+    /* update GroupMomentum & GroupParticlesNum & GroupKineticEnergy & GroupTemperature */
+    updateGroupProperties(md, MomentumSum, ParticlesNum, m_vSqd_Sum);
+
+    /* check if the particles must be placed in other cells or subdomains */
+    _fmd_refreshGrid(md);
+}
+
+static void SymplecticEuler_integrate(fmd_t *md, fmd_real_t duration)
+{
+    /* compute forces for the first time */
+    _fmd_dync_updateForces(md);
+
+    tick(md);
+
+    while (md->time < duration)
     {
-        md->ActiveGroup = GroupID;
-        _fmd_compute_GroupTemperature_etc_localgrid(md);
+        /* update positions and velocities */
+        SymplecticEuler_takeOneStep(md);
+
+        /* compute forces */
+        _fmd_dync_updateForces(md);
+
+        incTime(md);
     }
-    else
-        md->ActiveGroup = GroupID;
+    /* end of the time loop */
+}
 
-    md->timestep = timestep;
-
+static void VelocityVerlet_integrate(fmd_t *md, fmd_real_t duration, fmd_bool_t UseThermostat)
+{
     /* compute forces for the first time */
     _fmd_dync_updateForces(md);
 
@@ -167,7 +231,7 @@ void fmd_dync_integrate(fmd_t *md, int GroupID, fmd_real_t duration, fmd_real_t 
     while (md->time < duration)
     {
         /* take first step of velocity Verlet integrator */
-        VelocityVerlet_startStep(md, FMD_FALSE);
+        VelocityVerlet_startStep(md, UseThermostat);
 
         /* compute forces */
         _fmd_dync_updateForces(md);
@@ -178,6 +242,31 @@ void fmd_dync_integrate(fmd_t *md, int GroupID, fmd_real_t duration, fmd_real_t 
         incTime(md);
     }
     /* end of the time loop */
+}
+
+static void setActiveGroup(fmd_t *md, int GroupID)
+{
+    if (GroupID != md->ActiveGroup && md->ActiveGroup != FMD_GROUP_ALL)
+    {
+        md->ActiveGroup = GroupID;
+        _fmd_compute_GroupTemperature_etc_localgrid(md);
+    }
+    else
+        md->ActiveGroup = GroupID;
+}
+
+void fmd_dync_integrate(fmd_t *md, int GroupID, fmd_real_t duration, fmd_real_t timestep)
+{
+    if (!md->ParticlesDistributed) _fmd_matt_distribute(md);
+
+    setActiveGroup(md, GroupID);
+
+    md->timestep = timestep;
+
+    if (md->active_ttm_turi != NULL)
+        SymplecticEuler_integrate(md, duration);
+    else
+        VelocityVerlet_integrate(md, duration, FMD_FALSE);
 }
 
 void fmd_dync_equilibrate(fmd_t *md, int GroupID, fmd_real_t duration,
@@ -196,33 +285,9 @@ void fmd_dync_equilibrate(fmd_t *md, int GroupID, fmd_real_t duration,
     md->DesiredTemperature = temperature;
     md->BerendsenThermostatParam = strength;
 
-    if (GroupID != md->ActiveGroup && md->ActiveGroup != FMD_GROUP_ALL)
-    {
-        md->ActiveGroup = GroupID;
-        _fmd_compute_GroupTemperature_etc_localgrid(md);
-    }
-    else
-        md->ActiveGroup = GroupID;
+    setActiveGroup(md, GroupID);
 
-    // compute forces for the first time
-    _fmd_dync_updateForces(md);
-
-    tick(md);
-
-    while (md->time < duration)
-    {
-        // take first step of velocity Verlet integrator
-        VelocityVerlet_startStep(md, FMD_TRUE);
-
-        // compute forces
-        _fmd_dync_updateForces(md);
-
-        // take last step of velocity Verlet integrator
-        VelocityVerlet_finishStep(md);
-
-        incTime(md);
-    }
-    // end of the time loop
+    VelocityVerlet_integrate(md, duration, FMD_TRUE);
 
     // restore backups
     md->time = bak_time;
